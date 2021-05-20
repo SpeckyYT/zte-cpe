682b1aad3455e67f4334598e1d461446fc2453dd
SpeckyYT <Specky.fans@gmail.com>
2021-05-20 11:15:21 +0200

    set default values to utils

--- src/util.js
+++ src/util.js
@@ -1,24 +1,24 @@
 module.exports = {
-    contentToString(content){
+    contentToString(content = ''){
         const matches = content.match(/([0-9a-f]{1,4})/gi) || [];
         let string = '';
         for(const match of matches)
             string += eval(`"\\u${match.padStart(4, '0')}"`);
         return string;
     },
-    stringToContent(string){
+    stringToContent(string = ''){
         let content = '';
         for(let i = 0; i < string.length; i++)
             content += string.codePointAt(i).toString(16).padStart(4, '0');
         return content.toUpperCase();
     },
-    stringToBase64(string){
+    stringToBase64(string = ''){
         return Buffer.from(string,'utf-8').toString('base64');
     },
-    base64ToString(base64){
+    base64ToString(base64 = ''){
         return Buffer.from(base64,'base64').toString('utf-8');
     },
-    timeToDate(time, separator = ','){
+    timeToDate(time = '', separator = ','){
         const [
             year, month, day,
             hour, minute, second,
@@ -26,7 +26,7 @@ module.exports = {
         ] = time.split(separator).map(data => parseInt(data));
         return new Date(2000+year,month,day,hour,minute,second);
     },
-    dateToTime(date, separator = ','){
+    dateToTime(date = new Date(), separator = ','){
         return [
             date.getFullYear() - 2000,
             date.getMonth(),
